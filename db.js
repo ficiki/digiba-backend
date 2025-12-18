47bc26e88f1b5f140137245d180e9eee8acbb876
ficiki <fikiadsa@gmail.com>
2025-12-18 16:36:54 +0800

    Update db.js

--- db.js
+++ db.js
@@ -2,16 +2,15 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'https://mysql-production-e0d40.up.railway.app',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'digital',
+  password: process.env.DB_PASSWORD || 'uZAbkXAAVpIrBQWXVRsJdbfrQceTxnhk',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   enableKeepAlive: true
-  // JANGAN tambah keepAliveInitialDelayMs
 });
 
   pool.getConnection()
