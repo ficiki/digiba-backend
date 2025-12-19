9645ea2cd5bf06ff5bff68aee42e91120f6c91f5
ficiki <fikiadsa@gmail.com>
2025-12-19 23:14:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
